956412cdc2d02900c0091010ff602a6ed472b832
david <pavius.david@gmail.com>
2017-07-26 21:29:48 +0300

    Add method to get nodes directions

--- Board.py
+++ Board.py
@@ -4,11 +4,21 @@ import BoardUtil
 class Board:
 
     def __init__(self):
+        self.directions = ["northwest", "north", "northeast", "southeast", "south", "southwest"]
         self.lands_nodes = BoardUtil.fill_lands_nodes()
         self.edges = self.fill_edges()
 
     def get_lands_by_node(self, node):
         return [i for i, l in enumerate(self.lands_nodes) if node in l]
+
+    def get_node_directions(self, node):
+        node_directions = dict()
+        for i, l in enumerate(self.lands_nodes):
+            if node in l:
+                index = l.index(node)
+                node_directions[i] = self.directions[index]
+
+        return node_directions
 
     def fill_edges(self):
         edges = list()
